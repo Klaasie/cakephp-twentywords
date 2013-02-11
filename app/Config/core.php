3f98735f1fd504cf486f47e6c5588a9849f41913
Klaas Poortinga <poortinga.k@gmail.com>
2013-02-11 15:25:07 +0100

    changed security salts

--- app/Config/core.php
+++ app/Config/core.php
@@ -191,12 +191,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'ltos7oYyQwKN4LH7eCyYjqHKElcds3JjBZJXehYy');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '86762762372464500518235051912');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
